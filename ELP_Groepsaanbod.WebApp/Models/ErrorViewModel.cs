47d49aeb5e375724580b86dcc8e3b4784c4a8f73
Matthias Soenen <165031@student.creo.be>
2024-09-13 11:36:05 +0200

    001_Add Persons

---
+++ ELP_Groepsaanbod.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELP_Groepsaanbod.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
